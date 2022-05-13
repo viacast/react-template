2a22597dadf46a4ccd99376683d1eda13d57ad23
gabrieljablonski <gabriel@viacast.tv>
2022-05-13 15:08:29 -0300

    i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'services/i18n';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback={<div />}>
+      <App />
+    </React.Suspense>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,4 @@
-export { };
+import i18next from './i18n';
+
+// eslint-disable-next-line import/prefer-default-export
+export { i18next };
